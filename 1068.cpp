816cdbd6771b40bcebe61f7ef502fc2f9f26231b
Nevado <simon.tum@outlook.com>
2024-08-19 20:29:15 -0300

    bluegh

--- 1068.cpp
+++ 1068.cpp
@@ -2,7 +2,36 @@
 
 using namespace std;
 
+
 int main () {
-    string input;
-    int counter; 
-}
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string equation;
+    int ocount = 0, ccount = 0;
+    bool check = 1; 
+    do
+    {
+        getline(cin,equation);
+        for (int i = 0; i < equation.size(); i++) {
+            if (equation[i] == '(') 
+            {
+                ocount++;
+            }
+            if (equation[i] == ')') 
+            {
+                ccount++;
+                if(ccount > ocount) {
+                    check = 0;
+                    break;
+                }
+            }
+        }
+        if (ccount != ocount) check = 0;
+        cout << ((check)? "correct":"incorrect") << endl;
+        equation.clear();
+        check = 1;
+        ccount = 0;
+        ocount = 0;
+    } while (equation != " ");
+
+} 
