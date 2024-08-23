43fdb00c02dea83589048e2055b3d58bbbae0577
SimTum <simon.tum@outlook.com>
2024-08-22 21:51:43 -0300

    Data structure is impossible and I hate it

--- 1063.cpp
+++ 1063.cpp
@@ -2,41 +2,54 @@
 
 using namespace std;
 
-struct {
-    int index;
-    int desired_index;
-} train_car;
 
 int main () {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    vector <char> init;
-    vector <char> desired;
-    vector <char> buffer;
-    vector <char> result;
+    queue <char> init;
+    queue <char> desired;
+    stack <char> buffer;
+    stack <char> result;
     unsigned int t_length;
     string output = "";
-    cin >> t_length;
-    if (t_length != 0) {
+    do { 
+        cin >> t_length;
+        if (t_length < 0) return 0;
         for (int i = 0; i < t_length; i++) {
             char letter;
             cin >> letter;
-            init.push_back(letter);
+            init.push(letter);
             // cout << init [i] << " ";
         }
         for (int i = 0; i < t_length; i++) {
             char letter;
             cin >> letter;
-            desired.push_back(letter);
+            desired.push(letter);
             // cout << desired [i] << " ";
         }
-    }
+    int j = 0;
     for (int i = 0; i < t_length; i ++) {
-        buffer.push_back(init[1]);
-        init.erase(1);
-        if (buffer.end() == Desired [i]) {
+        buffer.push(init.front());
+        init.pop(); 
+        // cout << buffer[i] << " ";
+        output.append("I");
+        while (buffer.top() == desired.front() && desired.front() != 0) {
+            result.push(buffer.top());
+            buffer.pop();
+            desired.pop();
+            output.append("R");
+            // cout <<  result.top() << " ";
+            j++;
         }
-        else {
-        }
+    j = 0;
     }
+    if (output.size() != t_length * 2) cout << output << " Impossible" << endl;
+    else cout << output << endl;
+    init.empty();
+    desired.empty();
+    buffer.empty();
+    result.empty();
+    output.clear();
+    } while (t_length > 0);
+    return 0;
 }
